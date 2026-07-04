ede78a83691f6ec1a7632df5e1bae098698c6d03
NotTushar.dotcom <tushh45@gmail.com>
2026-07-04 09:11:04 +0530

    feat: add Drizzle DB connection, configure ESLint, and implement HttpClient utility

--- app/server/queries/connection.ts
+++ app/server/queries/connection.ts
@@ -1,4 +1,5 @@
 import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2";
 import { env } from "../lib/env.js";
 import * as schema from "../../db/schema.js";
 import * as relations from "../../db/relations.js";
@@ -9,7 +10,15 @@ let instance: ReturnType<typeof drizzle<typeof fullSchema>>;
 
 export function getDb() {
   if (!instance) {
-    instance = drizzle(env.databaseUrl, {
+    const pool = mysql.createPool(env.databaseUrl);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (pool as any).on("error", (err: Error) => {
+      console.error("MySQL Pool Error:", err);
+    });
+
+    // @ts-expect-error - Type mismatch between drizzle-orm and mysql2 versions
+    instance = drizzle(pool.promise(), {
       mode: "planetscale",
       schema: fullSchema,
     });
